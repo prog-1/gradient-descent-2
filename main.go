5b04ed59d426cd8c3bd5c40119234947ae488a3f
Alex <stukalovsaleksandrs@gmail.com>
2024-01-28 20:05:30 +0200

    Replaced inputs with types

--- main.go
+++ main.go
@@ -17,53 +17,48 @@ import (
 const (
 	screenWidth, screenHeight = 720, 480
 	randMin, randMax          = 1500, 2000
-	epochs, weightCount       = 1e6, 6
+	epochs, typeCount         = 1e6, 5
+	lrw, lrb                  = 0.1e-3, 0.7e-3
 )
 
-const lrb = 0.7e-3
-
-var learningRates = [weightCount]float64{0.1e-3, lrb, lrb, lrb, lrb, lrb}
+var learningRates = [typeCount + 1]float64{0.1e-3, lrb, lrb, lrb, lrb, lrb}
 
 // Prediction(inference) for one argument
-func p(x, w [weightCount]float64) float64 {
-	return w[0]*x[0] + w[1]*x[1] + w[2]*x[2] + w[3]*x[3] + w[4]*x[4] + w[5]*x[5]
-}
-
-// Runs model on all the input data
-func inference(xs [][weightCount]float64, w [weightCount]float64) (ys []float64) {
-	for i := range xs {
-		ys = append(ys, p(xs[i], w))
+func p(x float64, t [typeCount]float64, w [typeCount + 1]float64) (y float64) {
+	y = x * w[0]
+	for i := range t {
+		y += w[i+1] * t[i]
 	}
 	return
 }
 
-func loss(labels, y []float64) float64 {
-	var errSum float64
-	for i := range labels {
-		errSum += (y[i] - labels[i]) * (y[i] - labels[i])
+// Runs model on all the input data
+func inference(xs []float64, types [][typeCount]float64, w [typeCount + 1]float64) (ys []float64) {
+	for i := range xs {
+		ys = append(ys, p(xs[i], types[i], w))
 	}
-	return errSum / float64(len(labels)) // For the sake of making numbers smaller -> better percievable
+	return
 }
 
-func gradient(labels, y, x []float64, inputs [][weightCount]float64) (dw [weightCount]float64) {
-	// dw, db - Parial derivatives, w - weight, b - bias
+func gradient(labels, y, x []float64, types [][typeCount]float64) (ds [typeCount + 1]float64) {
+	// ds - weight partial DerivativeS
 	for i := 0; i < len(labels); i++ {
 		dif := y[i] - labels[i]
-		dw[0] += dif * x[0]
+		ds[0] += dif * x[0]
 	}
 
-	for t := 1; t < weightCount; t++ {
-		for i := 0; i < len(labels); i++ {
-			if inputs[i][t] == 1 {
+	for i := 0; i < len(labels); i++ {
+		for t := 0; t < typeCount; t++ {
+			if types[i][t] == 1 {
 				dif := y[i] - labels[i]
-				dw[t] += dif * x[t]
+				ds[t+1] += dif * x[t]
 			}
 		}
 	}
 
 	n := float64(len(labels))
-	for i := range dw {
-		dw[i] *= 2 / n
+	for i := range ds {
+		ds[i] *= 2 / n
 	}
 
 	return
@@ -128,50 +123,50 @@ func readHousesFromCSV(path string) ([]House, error) {
 }
 
 func main() {
-	houses, err2 := readHousesFromCSV("data/house_prices.csv")
-	if err2 != nil {
-		log.Fatalf("Can't read Houses from CSV: %v", err2)
+	houses, err := readHousesFromCSV("data/house_prices.csv")
+	if err != nil {
+		log.Fatalf("Can't read Houses from CSV: %v", err)
 	}
 
-	var inputs [][weightCount]float64
+	var types [][typeCount]float64
 	var squares []float64
 	var labels []float64
 	var points plotter.XYs
 	for i, house := range houses {
-		inputs = append(inputs, func() (res [weightCount]float64) {
+		labels = append(labels, house.Price)
+		squares = append(squares, house.Square)
+		points = append(points, plotter.XY{X: squares[i], Y: labels[i]})
+		types = append(types, func() (res [typeCount]float64) {
 			switch house.Type {
 			case "Duplex":
-				return [weightCount]float64{house.Square, 1, 0, 0, 0, 0}
+				return [typeCount]float64{1, 0, 0, 0, 0}
 			case "Detached":
-				return [weightCount]float64{house.Square, 0, 1, 0, 0, 0}
+				return [typeCount]float64{0, 1, 0, 0, 0}
 			case "Townhouse":
-				return [weightCount]float64{house.Square, 0, 0, 1, 0, 0}
+				return [typeCount]float64{0, 0, 1, 0, 0}
 			case "Semi-detached":
-				return [weightCount]float64{house.Square, 0, 0, 0, 1, 0}
+				return [typeCount]float64{0, 0, 0, 1, 0}
 			case "Multi-family":
-				return [weightCount]float64{house.Square, 0, 0, 0, 0, 1}
+				return [typeCount]float64{0, 0, 0, 0, 1}
 			default:
 				log.Fatalf("Unknown house type: %v", house.Type)
 			}
 			return
 		}())
-		labels = append(labels, house.Price)
-		squares = append(squares, house.Square)
-		points = append(points, plotter.XY{X: inputs[i][0], Y: labels[i]})
 	}
 
 	img := make(chan *image.RGBA, 1)
 	pointScatter, _ := plotter.NewScatter(points)
 
 	go func() {
-		var weights [weightCount]float64
+		var weights [typeCount + 1]float64
 		for i := range weights {
 			weights[i] = randMin + rand.Float64()*(randMax-randMin)
 		}
-		var weightDerivatives [weightCount]float64 // Weight derivatives = Values of gradient projection onto the weight axis
+		var weightDerivatives [typeCount + 1]float64 // Weight derivatives = Values of gradient projection onto the weight axis
 		for epoch := 0; epoch < epochs; epoch++ {
-			weightDerivatives = gradient(labels, inference(inputs, weights), squares, inputs)
-			for j := 0; j < weightCount; j++ {
+			weightDerivatives = gradient(labels, inference(squares, types, weights), squares, types)
+			for j := 0; j < typeCount+1; j++ {
 				weights[j] -= weightDerivatives[j] * learningRates[j]
 			}
 			if epoch%100 == 0 {
