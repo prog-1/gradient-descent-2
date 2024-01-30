84a7b2006953848e864e7506e7e91965f89ceeec
Alex <stukalovsaleksandrs@gmail.com>
2024-01-30 20:51:48 +0200

    Replaced types with wall colours

--- main.go
+++ main.go
@@ -12,15 +12,16 @@ import (
 )
 
 const (
-	screenWidth, screenHeight = 720, 480
-	randMin, randMax          = 1500, 2000
-	epochs, typeCount         = 1e6, 5
-	lrw                       = 0.1e-2
+	screenWidth, screenHeight       = 720, 480
+	randMin, randMax                = 1500, 2000
+	epochs, typeCount               = 1e6, 5
+	lrw                             = 0.1e-2
+	useWallColours, wallColourCount = true, typeCount
 )
 
 var typeColors [typeCount]color.RGBA = [typeCount]color.RGBA{color.RGBA{255, 0, 0, 255}, color.RGBA{0, 255, 255, 255}, color.RGBA{0, 255, 0, 255}, color.RGBA{100, 200, 0, 255}}
 
-var learningRates = [typeCount * 2]float64{lrw, lrw, lrw, lrw, lrw, 0.8e-1, 0.9, 0.5e-1, 0.9, 0.8e-1}
+var learningRates = [typeCount * 2]float64{lrw, 0.1e-9, lrw, lrw, lrw, 0.8e-1, 0.9e-3, 0.5e-1, 0.9, 0.8e-1}
 
 // Prediction(inference) for one argument
 func p(x float64, t [typeCount]float64, w [typeCount * 2]float64) (y float64) {
@@ -67,6 +68,7 @@ func main() {
 	var types [][typeCount]float64
 	var squares []float64
 	var labels []float64
+	var wallColours [][wallColourCount]float64
 	var points [typeCount]plotter.XYs
 	for i, house := range houses {
 		labels = append(labels, house.Price)
@@ -88,11 +90,31 @@ func main() {
 			}
 			return
 		}())
+		wallColours = append(wallColours, func() (selectors [wallColourCount]float64) {
+			switch house.WallColor {
+			case "blue":
+				return [wallColourCount]float64{1, 0, 0, 0, 0}
+			case "brown":
+				return [wallColourCount]float64{0, 1, 0, 0, 0}
+			case "white":
+				return [wallColourCount]float64{0, 0, 1, 0, 0}
+			case "green":
+				return [wallColourCount]float64{0, 0, 0, 1, 0}
+			case "yellow":
+				return [wallColourCount]float64{0, 0, 0, 0, 1}
+			}
+			return
+
+		}())
 		for t := 0; t < typeCount; t++ {
 			if types[i][t] == 1 {
 				points[t] = append(points[t], plotter.XY{X: squares[i], Y: labels[i]})
 			}
 		}
+	}
+
+	if useWallColours {
+		types = wallColours
 	}
 
 	img := make(chan *image.RGBA, 1)
